68c292ed49015acf57d841f0b7d3925624a3be49
prasaSL <prasadmadushan09@gmail.com>
2025-01-04 19:47:16 +0530

    Wrap application in AuthProvider for improved authentication context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
-
 'use client';
 
 import { ApolloProvider } from '@apollo/client';
 import Navbar from './components/navbar';
+import AuthProvider from './components/authProvider';
 import client from './graphql/client';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-      <ApolloProvider client={client}>
-          <Navbar /> {/* Global navigation bar */}
-          <main>{children}</main>
+        <ApolloProvider client={client}>
+          <AuthProvider>
+            <Navbar />
+            <main>{children}</main>
+          </AuthProvider>
         </ApolloProvider>
       </body>
     </html>
